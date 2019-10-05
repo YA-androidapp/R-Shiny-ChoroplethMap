9d7ff071220f88430e069626c3ce611bad175410
YA <YA-androidapp@users.noreply.github.com>
2019-10-05 23:13:16 +0900

    Update app.R

--- app.R
+++ app.R
@@ -7,10 +7,11 @@
 
 inst <- function(x)
 {
-  if (!require(x,character.only = TRUE))
+  if (!require(x, character.only = TRUE))
   {
-    install.packages(x,dep=TRUE)
-    if(!require(x,character.only = TRUE)) stop("Package not found")
+    install.packages(x, dep = TRUE)
+    if (!require(x, character.only = TRUE))
+      stop("Package not found")
   }
 }
 
@@ -31,42 +32,42 @@ data_path = "H30.csv"
 # Proc
 shape <-
   readOGR(shape_path, stringsAsFactors = FALSE, encoding = "UTF-8")
-shape@data$市区町丁  <-
+shape@data$市区町丁   <-
   paste0(shape@data$CITY_NAME, ifelse(is.na(shape@data$S_NAME), "", shape@data$S_NAME))
 head(shape@data)
 
 
 
 # Define UI for application that draws a histogram
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-        leafletOutput("mymap"),
-        p(),
-        actionButton("recalc", "New points"),
-        getwd()
-      )
-   )
-)
+ui <- fluidPage(# Application title
+  titlePanel("Old Faithful Geyser Data"),
+  
+  # Sidebar with a slider input for number of bins
+  sidebarLayout(
+    sidebarPanel(sliderInput(
+      "bins",
+      "Number of bins:",
+      min = 1,
+      max = 50,
+      value = 30
+    )),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      leafletOutput("mymap"),
+      p(),
+      actionButton("recalc", "New points"),
+      getwd()
+    )
+  ))
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-
-  data_density <- eventReactive(input$recalc, {
+  data_density <- NULL
+  color_pallet <- NULL
+  labels <- NULL
+  
+  color_pallet_reactive <- eventReactive(input$recalc, {
     datacsv <-
       read.csv(data_path,
                stringsAsFactors = FALSE,
@@ -88,62 +89,53 @@ server <- function(input, output, session) {
     crimecase_density <-
       as.numeric(joined$総合計) / as.numeric(shape@data$JINKO) * 100000 # 人口100000人当たり認知件数
     
-    # data_density <- population_density
-    # data_density <- household_density
-    data_density <- crimecase_density
-    
-    
     # 欠損値(NA)を0で置換する
-    data_density[is.na(data_density)] <- 0
+    crimecase_density[is.na(crimecase_density)] <- 0
+    data_density <<- crimecase_density
     
     color_pallet <-
-      # colorNumeric("Blues", domain = data_density, reverse = F) # 連続量を塗り分け
       colorQuantile("Blues",
                     domain = data_density,
                     reverse = F,
                     n = 8) # 分位数で塗り分け
     
-    labels <- sprintf("<strong>%s</strong><br/>%5.1f",
-                      paste0(joined$MOJI),
-                      data_density) %>% lapply(htmltools::HTML)
+    labels <<- sprintf("<strong>%s</strong><br/>%5.1f",
+                       paste0(joined$MOJI),
+                       data_density) %>% lapply(htmltools::HTML)
     
-    return(cbind(shape@data$X_CODE, shape@data$Y_CODE))
+    return(color_pallet)
   }, ignoreNULL = FALSE)
   
   output$mymap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)
+    color_pallet <- color_pallet_reactive()
+    shape %>%
+      leaflet() %>%
+      setView(lat = 35.65,
+              lng = 139.75,
+              zoom = 12) %>% # 初期表示
+      addProviderTiles(providers$CartoDB.Positron) %>% # ベースマップ
+      addPolygons(
+        fillOpacity = 0.7,
+        weight = 1,
+        color = "#666",
+        fillColor = ~ color_pallet(data_density),
+        label = labels,
+        labelOptions = labelOptions(
+          style = list("font-weight" = "normal", padding = "3px 8px"),
+          textsize = "15px",
+          direction = "auto"
+        )
       ) %>%
-      addMarkers(data = data_density())
-    
-    # shape %>%
-    #   leaflet() %>%
-    #   setView(lat = 35.65, lng = 139.75, zoom = 12) %>% # 初期表示
-    #   addProviderTiles(providers$CartoDB.Positron) %>% # ベースマップ
-    #   addPolygons(
-    #     fillOpacity = 0.7,
-    #     weight = 1,
-    #     color = "#666",
-    #     fillColor = ~ color_pallet(data_density),
-    #     label = labels,
-    #     labelOptions = labelOptions(
-    #       style = list("font-weight" = "normal", padding = "3px 8px"),
-    #       textsize = "15px",
-    #       direction = "auto"
-    #     )
-    #   ) %>%
-    #   # 凡例
-    #   addLegend(
-    #     "bottomright",
-    #     pal = color_pallet,
-    #     values = ~ data_density,
-    #     title = "人口100000人当たり認知件数"
-    #   )
+      # 凡例
+      addLegend(
+        "bottomright",
+        pal = color_pallet,
+        values = ~ data_density,
+        title = "人口100000人当たり認知件数"
+      )
   })
   
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
-
